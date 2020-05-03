c3c85528b605fe2d78c20b6f37864d3de1a2ae44
aSmartTortoise <wyjonneteasy@163.com>
2020-05-03 15:01:54 +0800

    Hello world

---
+++ kotlinTutorials/kt02/src/com/kotlin/wyj/Main.kt
@@ -0,0 +1,10 @@
+package com.kotlin.wyj
+
+fun main(args: Array<out String>) {
+    println("Hello world!")
+    println(Main(0, "frankie"))
+}
+
+data class Main(val id: Int, var name : String) {
+
+}
