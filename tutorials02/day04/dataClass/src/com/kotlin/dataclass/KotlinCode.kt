abe2811b7d28a6a94b05e9009b8b6e06fee9a6ca
aSmartTortoise <wyjonneteasy@163.com>
2020-06-27 20:15:49 +0800

    data class

---
+++ tutorials02/day04/dataClass/src/com/kotlin/dataclass/KotlinCode.kt
@@ -0,0 +1,16 @@
+package com.kotlin.dataclass
+
+fun main(args: Array<String>) {
+    val studengA = Student("Tommy", 102)
+    println(studengA)
+    var studentB = studengA.copy()
+    println(studengA == studentB)
+}
+
+data class Student(val name: String, val roomNo: Int) {
+    val grade: Int = 12
+    
+    fun printGrade() {
+        println("Grade:$grade")
+    }
+}
