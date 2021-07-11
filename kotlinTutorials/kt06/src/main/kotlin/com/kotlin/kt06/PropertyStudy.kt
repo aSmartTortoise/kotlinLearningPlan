b71cafbd64f06662ecfed6051d647a46b240bd9f
aSmartTortoise <wyjonneteasy@163.com>
2021-07-11 11:56:42 +0800

    属性的幕后字段再学习

--- kotlinTutorials/kt06/src/main/kotlin/com/kotlin/kt06/PropertyStudy.kt
+++ kotlinTutorials/kt06/src/main/kotlin/com/kotlin/kt06/PropertyStudy.kt
@@ -6,10 +6,10 @@ package com.kotlin.kt06
  *          [<getter>]
  *          [<setter>]
  *      其初始化器initializer、getter和setter都是可选书写的。
- *      如果我们声明属性的时候自定义getter，那么每次访问这个属性的时候都会调用ta的get函数。
+ *      如果我们声明属性的时候自定义getter，那么每次访问这个属性的时候都会调用它的get函数。
  * 2 幕后字段 https://juejin.cn/post/6844903673353486343
- *      如果属性至少一个访问器使用了默认实现，那么Kotlin会自动提供幕后字段，用field表示，
- *      幕后字段只能用于访问器中。幕后字段field就是指当前的属性，在getter和setter这两个
+ *      如果属性至少一个访问器使用了默认实现，那么Kotlin会自动提供幕后字段，用关键字field表示，
+ *      幕后字段只能用于自定义的getter、setter访问器中。幕后字段field就是指当前的属性，在getter和setter这两个
  *      特殊的作用域中有特殊的意义，类似于一个类中的this就代表这个类。
  * 2.1 有幕后字段的属性，转换成Java代码后一定有一个与之对应的成员变量，反之则没有。
  * 3 编译期常量
@@ -61,6 +61,9 @@ class Address {
 }
 
 class PersonClass(var name: String) {
+    /**
+     * 可变属性age的访问器getter、setter的默认实现，是有幕后字段的。
+     */
     var age: Int = 0
         get() = field
         set(value) {
