6b0dec44502781980e749feca4ecae2729cdb9df
aSmartTortoise <wyjonneteasy@163.com>
2021-08-01 15:25:39 +0800

    协程的取消和超时；cancel、cancelAndJoin、isActive、finally释放资源

--- kotlinTutorials/coroutine/src/main/kotlin/com/kotlin/coroutine/example-basic-01.kt
+++ kotlinTutorials/coroutine/src/main/kotlin/com/kotlin/coroutine/example-basic-01.kt
@@ -1,6 +1,7 @@
 package com.kotlin.coroutine
 
 import kotlinx.coroutines.*
+import org.omg.PortableInterceptor.ACTIVE
 import kotlin.concurrent.thread
 
 /**
@@ -12,6 +13,13 @@ import kotlin.concurrent.thread
  *      除了由不同的协程构建器提供协程作用域外，还可以使用coroutineScope构建器声明自己的作用域，它会创建一个
  *  协程作用域，并在其他已启动的协程执行完毕之前不会结束。
  *
+ *      协程较线程比较轻量。全局协程像守护线程。
+ *  18.2 取消和超时
+ *      lauch函数返回了一个可以取消运行中的协程的Job。
+ *      一段协程代码只有是协做的才能被取消。但是协程做的是计算任务，并且没有检查取消的话，它是不能被取消的。
+ *      isActive是CoroutineScope的一个扩展属性，在协程中恰当使用该属性可以使得该协程是协作的，是可取消的。
+ *      在finally中释放资源。通常使用try finally代码块处理协程被取消的时候，可被取消的挂起函数抛出的
+ *      CancellationException。
  *
  *
  *
@@ -35,23 +43,118 @@ import kotlin.concurrent.thread
 //    println("hello world, ")
 //}
 
-fun main() = runBlocking {
-    launch {
-        delay(200L)
-        println("Task from runBlocing!")
-    }
-
-    coroutineScope {
-        launch {
+//fun main() = runBlocking {
+//    launch {
+//        suspendFuctionStudy()
+//    }
+//
+//    coroutineScope {
+//        launch {
 //            delay(500L)
-            println("Task from nested launch")
-        }
+//            println("Task from nested launch")
+//        }
+//
+//        delay(100L)
+//        println("Task from coroutin scope!")
+//    }
+//
+//    println("Coroutine scope is over!")
+//}
 
-        delay(100L)
-        println("Task from coroutin scope!")
+//fun main() = runBlocking {
+//    GlobalScope.launch {
+//        repeat(1000) {
+//            println("I am sleeping $it")
+//            delay(500L)
+//        }
+//    }
+//
+//    delay(1300L)
+//}
+
+//fun main() = runBlocking {
+//    val job = launch {
+//        repeat(1000) {
+//            println("job i am sleeping $it")
+//            delay(500L)
+//        }
+//    }
+//
+//    delay(1300L)
+//    println("i am tired of waiting!")
+//    //取消正在活动中的协程
+//    job.cancel()
+//    job.join()
+//    println("now i am quit!")
+//}
+
+//fun main() = runBlocking {
+//    val startTime = System.currentTimeMillis()
+//    val job = launch(Dispatchers.Default) {
+//        var nextPrintTime = startTime
+//        var i = 0
+//        while (i < 15) {
+//            if (System.currentTimeMillis() >= nextPrintTime) {
+//                println("job, i am sleeping ${i++}")
+//                nextPrintTime += 500L
+//            }
+//        }
+//    }
+//
+//    println("main thread will be blocked")
+//    delay(1300L)
+//    println("i am tired of waiting!")
+//    job.cancelAndJoin()
+//    // 一段协程代码只有是协做的才能被取消。但是协程做的是计算任务，并且没有检查取消的话，它是不能被取消的。
+//    println("now i am quit!")
+//}
+
+//fun main() = runBlocking {
+//    val startTime = System.currentTimeMillis()
+//    val job = launch(Dispatchers.Default) {
+//        var nextPrintTime = startTime
+//        var i = 0
+//        while (isActive) {
+//            if (System.currentTimeMillis() >= nextPrintTime) {
+//                println("job, i am sleeping ${i++}")
+//                nextPrintTime += 500L
+//            }
+//        }
+//    }
+//
+//    println("main thread will to be blocked.")
+//    delay(1300L)
+//    println("main , i am tired of waiting.")
+//    job.cancelAndJoin()
+//    println("main, now i am out")
+//}
+
+fun main() = runBlocking {
+    val job = launch(Dispatchers.Default) {
+        try {
+            repeat(1000) {
+                println("job, i am sleeping $it")
+                delay(500L)
+            }
+        } catch (e: Exception) {
+            println("something is wrong e:" + e)
+        } finally {
+            println(" finally!")
+        }
     }
 
-    println("Coroutine scope is over!")
+    delay(2000L)
+    println("main, i am tired of waiting.")
+    job.cancelAndJoin()
+    println("main, now i am out.")
+}
+
+/**
+ *  挂起函数
+ */
+private suspend fun suspendFuctionStudy() {
+    delay(200L)
+    println("Task from runBlocing!")
 }
 
 private fun jobJoinStudy() {
