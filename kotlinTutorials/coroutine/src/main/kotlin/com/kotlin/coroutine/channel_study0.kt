74dbf7e208da56f64dfdf17ed0a882ae0b2d38d7
aSmartTortoise <wyjonneteasy@163.com>
2021-08-16 21:11:56 +0800

    Channel 扇入；缓冲；通道的发送和接收是公平的；计时器通道tickerChannel

--- kotlinTutorials/coroutine/src/main/kotlin/com/kotlin/coroutine/channel_study0.kt
+++ kotlinTutorials/coroutine/src/main/kotlin/com/kotlin/coroutine/channel_study0.kt
@@ -1,14 +1,14 @@
 package com.kotlin.coroutine
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.*
 
 /**
  *  18.8 通道
  *      和队列不同，一个通道可以通过被关闭来表明没有更多的元素进入通道。
+ *  18.8.1 带缓冲的管道
+ *      Channel构造方法和produce构建器通过一个可选的参数capacity来指定缓冲区的大小。缓冲允许通
+ *  道的发送者在挂起前发送多个元素。
  *
  *
  */
@@ -39,12 +39,103 @@ fun CoroutineScope.launchProcessors(id: Int, channel: ReceiveChannel<Int>) = lau
         println("processor #$id received $msg")
     }
 }
+
+suspend fun sendString(channel: SendChannel<String>, str: String, time: Long) {
+    while (true) {
+        delay(time)
+        channel.send(str)
+    }
+}
+
+data class Ball(var hits: Int)
+
+suspend fun player(name: String, table: Channel<Ball>) {
+    for (ball in table) {
+        ball.hits++
+        println("$name $ball")
+        delay(300L)
+        table.send(ball)
+    }
+}
 fun main() {
 //    channelStudy01()
 //    channelStudy02Close()
 //    channelStudy03ProduceConsume()
 //    channelStudy04()
-    channelStudy05()
+//    channelStudy05()
+//    channelStudy06()
+//    channelStudy07Capacity()
+//    channelStudy08()
+    channelStudy09TickerChannel()
+
+}
+
+private fun channelStudy09TickerChannel() {
+    runBlocking {
+        val ticker = ticker(delayMillis = 100L, initialDelayMillis = 0L)
+        var nextElement = withTimeoutOrNull(1L) { ticker.receive() }
+        println("initial element is $nextElement")
+        nextElement = withTimeoutOrNull(50L) { ticker.receive() }
+        println("next element in 50ms is $nextElement")
+        nextElement = withTimeoutOrNull(60L) { ticker.receive() }
+        println("next element in 110ms is $nextElement")
+        println("consume pause for 150ms")
+        delay(150L)
+        nextElement = withTimeoutOrNull(1L) { ticker.receive() }
+        println("next element after large consume delay is $nextElement")
+        nextElement = withTimeoutOrNull(60L) { ticker.receive() }
+        println("next element in 50ms after consume pause in 150ms is $nextElement")
+        ticker.cancel()
+    }
+}
+
+private fun channelStudy08() {
+    runBlocking {
+        val table = Channel<Ball>()
+        launch {
+            player("ping", table)
+        }
+        launch {
+            player("pong", table)
+        }
+        table.send(Ball(0))
+        delay(2000L)
+        coroutineContext.cancelChildren()
+    }
+}
+
+private fun channelStudy07Capacity() {
+    runBlocking {
+        val channel = Channel<Int>(4)
+        val job = launch {
+            repeat(10) {
+                println("send $it")
+                channel.send(it)
+            }
+        }
+        delay(1000L)
+        job.cancel()
+    }
+}
+
+/**
+ * 多个协程处理通道上游的发送事件
+ */
+private fun channelStudy06() {
+    runBlocking {
+        val channel = Channel<String>()
+        launch {
+            sendString(channel, "foo", 200L)
+        }
+        launch {
+            sendString(channel, "bar", 300L)
+        }
+
+        repeat(6) {
+            println(channel.receive())
+        }
+        coroutineContext.cancelChildren()
+    }
 }
 
 /**
