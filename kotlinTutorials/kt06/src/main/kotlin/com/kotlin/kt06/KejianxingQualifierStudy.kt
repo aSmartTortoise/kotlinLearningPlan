9d5cb02289be9586c9df00c879f71d5ea1461a88
aSmartTortoise <wyjonneteasy@163.com>
2021-08-29 20:30:28 +0800

    可见性修饰符、作用域函数之run

---
+++ kotlinTutorials/kt06/src/main/kotlin/com/kotlin/kt06/KejianxingQualifierStudy.kt
@@ -0,0 +1,9 @@
+package com.kotlin.kt06
+
+/**
+ *  可见性修饰符
+ *      类、对象、接口、构造函数、普通函数、属性以及属性的setter方法都可以由可见性修饰符。
+ *  属性的getter函数总是与属性有着相同的可见性。Kotlin有四个可见性修饰符：private、protectes
+ *  、internal和public。如果没有显示指定可见性的话，默认是public的。
+ *      internal同一模块内可见。
+ */
