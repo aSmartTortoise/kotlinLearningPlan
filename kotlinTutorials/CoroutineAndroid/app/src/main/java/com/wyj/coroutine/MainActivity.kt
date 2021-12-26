97852d3552c479b62e4ec0631e58d4db7af23b0b
aSmartTortoise <wyjonneteasy@163.com>
2021-12-26 11:22:23 +0800

    协程之CouroutineStart启动模式1

--- kotlinTutorials/CoroutineAndroid/app/src/main/java/com/wyj/coroutine/MainActivity.kt
+++ kotlinTutorials/CoroutineAndroid/app/src/main/java/com/wyj/coroutine/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.*
 
 /**
  *  https://juejin.cn/post/6953441828100112392
+ *  https://juejin.cn/post/6953287252373930021
  *
  *  1 CoroutineDispatcher
  *      协程调度器：调度器确定了相关的协程在哪个线程或哪些线程上运行。协程调度器可以限制协程在某个线程上运行、
@@ -18,6 +19,19 @@ import kotlinx.coroutines.*
  *  CoroutineExceptionHandler、ContinuationInterceptor、CoroutineName。这些都是Element的衍生类。
  *  这些类或接口都有一个CoroutineContext.Element类型的伴生对象Key。由CoroutineContext的集合根据对应的
  *  Key可以获取指定的Element。
+ *  3 CoroutineStart 协程启动模式
+ *      是一个枚举类，有四种状态。
+ *      DEFALUT 默认启动模式；协程创建之后，立即调度协程准备执行。虽然是立即调度准备执行，但并未开始执行，
+ *  协程仍然可以在执行前被取消。
+ *      LAZY 协程创建之后，不是立即调度协程准备执行，当调用start、join或await类似的方法的时候，才调度
+ *  协程准备执行。
+ *      ATOMIC 原子性的模式；当协程创建之后，原子性的立即调度协程准备执行，协程在执行到第一个挂起点之前
+ *  不会被取消。
+ *      UNDISPATCHED 非调度的模式；协程创建之后，不经过调度立即执行。协程在执行到第一个挂起点之前不会被
+ *  取消。与ATOMIC相同的地方是都会立即执行，在执行到第一个挂起点之前不会被取消。不同点在于UNDISPATCHED
+ *  模式不经过调度立即执行的。当然在遇到挂起点之后的执行，取决于挂起点本事的逻辑和协程上下文中的调度器。
+ *
+ *
  *
  */
 class MainActivity : AppCompatActivity() {
@@ -53,8 +67,76 @@ class MainActivity : AppCompatActivity() {
 //        }
 
 //        plusCoroutineContextElement()
+//        coroutineStartStudy()
+//        coroutineUndispatchedStudy()
+        coroutineUndispatchedStudy2()
 
+    }
 
+    /**
+     *  先执行ioJob协程体挂起点之前的代码，且挂起点之前的线程是在主线程，接着执行mainJob协程体中、join之前的代码
+     *  然后执行ioJob协程挂起点之后的代码，且挂起点之后的线程是子线程，最后执行mainJob join之后的代码。
+     *  先执行ioJob协程体挂起点之前的代码也说明了CoroutineStart.UNDISPATCHED的启动模式的协程是不经过调度立即
+     *  执行的。
+     *  由于CoroutineStart.UNDISPATCHED模式启动的协程是不经过调度立即执行的，所以在挂起点之前位于
+     *  主线程，由于ioJob的调度器是Dispatchers.IO所以挂起点之后是位于子线程的。
+     */
+    private fun coroutineUndispatchedStudy2() {
+        GlobalScope.launch(context = Dispatchers.Main) {
+            val ioJob = launch(context = Dispatchers.IO, start = CoroutineStart.UNDISPATCHED) {
+                Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程:${Thread.currentThread().name}, 挂起前")
+                delay(100L)
+                Log.d(
+                    TAG,
+                    "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, 挂起后。"
+                )
+            }
+            Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, join之前。")
+            ioJob.join()
+            Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, join之后。")
+        }
+    }
+
+    private fun coroutineUndispatchedStudy() {
+        GlobalScope.launch(context = Dispatchers.Main) {
+            val ioJob = launch(context = Dispatchers.IO) {
+                Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程:${Thread.currentThread().name}, 挂起前")
+                delay(100L)
+                Log.d(
+                    TAG,
+                    "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, 挂起后。"
+                )
+            }
+            Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, join之前。")
+            ioJob.join()
+            Log.d(TAG, "coroutineUndispatchedStudy: wyj 线程：${Thread.currentThread().name}, join之后。")
+        }
+    }
+
+    /**
+     *  undispatchedJob、atomicJob都执行了、lazyJob未执行，DefaultJob有可能没有执行。
+     *  undispatchedJob和atomicJob在挂起点之后都被取消，挂起点之后的代码未执行。
+     */
+    private fun coroutineStartStudy() {
+        val defaultJob = GlobalScope.launch {
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.DEFAULT.")
+        }
+        defaultJob.cancel()
+        val lazyJob = GlobalScope.launch(start = CoroutineStart.LAZY) {
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.LAZY.")
+        }
+        val atomicJob = GlobalScope.launch(start = CoroutineStart.ATOMIC) {
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.ATOMIC 挂起点之前。")
+            delay(100L)
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.ATOMIC 挂起点之后。")
+        }
+        atomicJob.cancel()
+        val undispatchedJob = GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.UNDISPATCHED 挂起点之前。")
+            delay(100L)
+            Log.d(TAG, "coroutineStartStudy: wyj CoroutineStart.UNDISPATCHED 挂起点之后。")
+        }
+        undispatchedJob.cancel()
     }
 
     private fun plusCoroutineContextElement() {
